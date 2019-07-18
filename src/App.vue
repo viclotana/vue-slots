4c6c543b18fb156d94c9961e0b1ced3bf481e5a1
mac <mac@macs-MacBook-Pro.local>
2019-07-18 17:09:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Test v-slot="{team}">
+      <h2>Hello world! my team is {{team}}</h2>
+      <!-- <p slot="paragraph">Hello, I am a paragraph text</p>
+      <ul slot="links">
+        <li>Hello, I am a list item</li>
+        <li>Hello, I am a list item</li>
+      </ul> -->
+    </Test>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <slot v-bind:team="team"></slot>
+    <slot name="paragraph"></slot>
+    <slot name="links"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Test',
+  data(){
+    return{
+      team:"FC Barcelona"
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
